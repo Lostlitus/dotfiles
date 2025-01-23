53911d1be1cc31db776e0d74f671153e4471f6da
Lotus <lotus_chuan@qq.com>
2025-01-23 14:47:51 +0800

    Add sanity check for Linux and remove useless ruby lsp

--- .script/install_zsh.sh
+++ .script/install_zsh.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
 git clone https://github.com/larkery/zsh-histdb.git $HOME/Developer/local/src/zsh-histdb
-
-brew install fzf; fzf --zsh > $HOME/.fzf.zsh
-
 git clone https://github.com/Aloxaf/fzf-tab $HOME/Developer/local/src/fzf-tab
 
-brew install zsh-autosuggestions
+if [ "$(uname)" == "Darwin" ]
+then
+    brew install fzf; fzf --zsh > $HOME/.fzf.zsh
+    brew install zsh-autosuggestions
+    brew install zsh-syntax-highlighting
+else
+    echo "Warning: Current OS is not macOS, install fzf manually is needed"
+    echo "Warning: Current OS is not macOS, install zsh-autosuggestions manually is needed"
+    echo "Warning: Current OS is not macOS, install zsh-syntax-highlighting manually is needed"
+fi
 
-brew install zsh-syntax-highlighting
+
+
